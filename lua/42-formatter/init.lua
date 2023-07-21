c3eb968f51bdf3b3c8e29b7124c56937e035a7dc
Diogo-ss <diogosantosdasilva6@gmail.com>
2023-07-21 18:10:18 -0300

    style: format lua files

--- lua/42-formatter/init.lua
+++ lua/42-formatter/init.lua
@@ -2,19 +2,19 @@ local M = {}
 local api = vim.api
 
 M.opts = {
-  formatter = 'c_formatter_42',
+  formatter = "c_formatter_42",
   filetypes = { c = true, h = true, cpp = true, hpp = true },
 }
 
 function M.set_equalprg(formatter)
   local current_buf = api.nvim_get_current_buf()
-  api.nvim_buf_set_option(current_buf, 'equalprg', formatter)
+  api.nvim_buf_set_option(current_buf, "equalprg", formatter)
 end
 
 M.format = function(filetypes, formatter)
   if filetypes[vim.bo.filetype] then
     M.set_equalprg(formatter)
-    api.nvim_feedkeys('gg=G', 'n', true)
+    api.nvim_feedkeys("gg=G", "n", true)
   else
     vim.notify("Unsupported file type", vim.log.levels.WARN)
   end
@@ -23,7 +23,7 @@ end
 M.setup = function(opts)
   M.opts = vim.tbl_deep_extend("force", M.opts, opts or {})
 
-  api.nvim_create_user_command('CFormat42', function()
+  api.nvim_create_user_command("CFormat42", function()
     M.format(M.opts.filetypes, M.opts.formatter)
   end, {})
 end
